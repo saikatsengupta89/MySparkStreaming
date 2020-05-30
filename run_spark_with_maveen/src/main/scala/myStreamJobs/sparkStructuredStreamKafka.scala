f9090bf13043da5e54c3ff6845c8e68800e45610
Satan Romeo <Satan Romeo@LAPTOP-QDFQKKCR>
2020-05-30 20:01:05 +0400

    Updated

--- run_spark_with_maveen/src/main/scala/myStreamJobs/sparkStructuredStreamKafka.scala
+++ run_spark_with_maveen/src/main/scala/myStreamJobs/sparkStructuredStreamKafka.scala
@@ -2,6 +2,7 @@ package myStreamJobs
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.streaming.Trigger
 
 import org.apache.spark.sql.streaming.OutputMode
 
@@ -25,7 +26,7 @@ object sparkStructuredStreamKafka {
                          .add("transaction_id",StringType)
                          .add("transaction_card_type",StringType)
                          .add("transaction_amount",DoubleType)
-                         .add("transacction_datetime",StringType)
+                         .add("transaction_datetime",TimestampType)
                          
         
         val kafkaStreamDF = spark.readStream
@@ -41,12 +42,28 @@ object sparkStructuredStreamKafka {
                                                   .cast("string"))
         
         
-        val kafkaStreamSTG2 = kafkaStreamSTG1.select(from_json(col("value"), schemaDef))
+        val kafkaStreamSTG2 = kafkaStreamSTG1.select(from_json(col("value"), schemaDef).alias("data"))
+                                             .select("data.*")
+                                             
+        
+        kafkaStreamSTG2.registerTempTable("transaction_data") 
+        
+        val kafkaStreamSTG3 = spark.sql (
+                                "select "+
+                                "transaction_datetime, "+
+                                "from_unixtime(unix_timestamp(transaction_datetime, 'yyyy-MM-dd HH:MM:SS'),'yyyyMMdd') as time_key, "+
+                                "transaction_id, "+
+                                "transaction_card_type, "+
+                                "transaction_amount, "+
+                                "case when transaction_amount > 500 then 'TXN GT THAN 500' else 'TXN WITHIN 500' end as txn_class "+
+                                "from transaction_data"
+                              )
                                    
-        val sink = kafkaStreamSTG2.writeStream
+        val sink = kafkaStreamSTG3.writeStream
                                   .format(source="console")
                                   .option("truncate", "false")
                                   .outputMode(OutputMode.Append())
+                                  .trigger(Trigger.ProcessingTime("20 seconds"))
                                   .start()
         
         sink.awaitTermination()
